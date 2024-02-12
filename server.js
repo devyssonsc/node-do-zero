41b8ef3e921dbe3e53cee01f682dd0ab59085972
Devysson Cardoso <devyssonsc@gmail.com>
2024-02-12 19:22:00 +0000

    database-memory

--- server.js
+++ server.js
@@ -9,12 +9,54 @@
 
 // server.listen(3333)
 
-import {fastify} from 'fastify'
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.get('/', () => {
-    return 'Hello World'
+const database = new DatabaseMemory()
+
+
+
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', (request) => {
+    const search = request.query.search
+
+    const videos = database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoID = request.params.id
+    const {title, description, duration} = request.body
+
+    database.update(videoID, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
