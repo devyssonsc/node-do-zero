fbbd5924541cc098e4e51dd704ec98b788caa359
Devysson Cardoso <devyssonsc@gmail.com>
2024-02-12 17:47:25 +0000

    fastify

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import {createServer} from 'node:http'
+// import {createServer} from 'node:http'
 
-const server = createServer((request, response) => {
+// const server = createServer((request, response) => {
     
-    response.write("oi")
+//     response.write("oi")
     
-    return response.end()
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import {fastify} from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Hello World'
 })
 
-server.listen(3333)
-
-//localhost:3333
+server.listen({
+    port: 3333,
+})
